11f3b9898ee0f202d999572e6fa77d3975a0c1bc
ajm1808 <ajm180885@gmail.com>
2020-05-05 12:20:56 +0100

    replace files

--- mc-5-1-js-dom-starterfiles/1 - Add Event Listener/app.js
+++
@@ -1,7 +0,0 @@
-const myHeading = document.getElementById("heading");
-//myHeading is a variable referring to the element in the document with ID "heading"
-myHeading.addEventListener("click",()=>{
-    myHeading.style.color = "purple";
-});
-
-//to that variable, adding addEventListener so that when it clicks it runs the function - this changes the color to purple when clicked.
