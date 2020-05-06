c4fed4aebf5e09e3a3b38031454005fbcc59ca18
ajm1808 <ajm180885@gmail.com>
2020-05-06 12:07:34 +0100

    updated 6

---
+++ mc-5-1-js-dom-starterfiles/6 - Changing Element Attributes/app.js
@@ -0,0 +1,8 @@
+const image=document.getElementById("image");
+
+const input=document.getElementById("input");
+const button=document.getElementById("submit");
+
+submit.addEventListener=("click",()=>{
+    image.src=input.value;
+})
