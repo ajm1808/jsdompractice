3f58dfbfe072a3441696ddc681723f728ca88ce9
ajm1808 <ajm180885@gmail.com>
2020-05-06 16:43:44 +0100

    challenge

---
+++ mc-5-1-js-dom-starterfiles/challenge/app.js
@@ -0,0 +1,11 @@
+
+// const newPara=document.createElement('p');
+// newPara.textContent='added with js';
+
+// document.querySelector('body').appendChild(newPara);
+
+function keycode(){
+    let key = event.keyCode;
+    console.log(key);
+    }
+
