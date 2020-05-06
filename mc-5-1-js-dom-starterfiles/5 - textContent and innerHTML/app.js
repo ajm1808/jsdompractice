7b64a2dcfa535a3a0492a297b24c18415417ca14
ajm1808 <ajm180885@gmail.com>
2020-05-06 11:48:33 +0100

    updated practice5

---
+++ mc-5-1-js-dom-starterfiles/5 - textContent and innerHTML/app.js
@@ -0,0 +1,14 @@
+const placeholder=document.getElementById("placeholder");
+const input=document.getElementById("input");
+const submit=document.getElementById("submit");
+const list=document.getElementById("list");
+
+submit.addEventListener("click",()=>{
+    placeholder.style.color="goldenrod";
+    placeholder.textContent=`<li>${input.value}</li>`;
+    list.innerHTML=`<li>${input.value}</li>`;
+})
+
+//textContent replaced the placeholder with everything after =
+//list.innerHTML replaces all list content
+
