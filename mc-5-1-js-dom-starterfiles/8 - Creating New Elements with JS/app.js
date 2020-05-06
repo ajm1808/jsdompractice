e42b040c499e16063a927ba43826fba45350444d
ajm1808 <ajm180885@gmail.com>
2020-05-06 15:18:28 +0100

    8

--- mc-5-1-js-dom-starterfiles/8 - Creating New Elements with JS/app.js
+++ mc-5-1-js-dom-starterfiles/8 - Creating New Elements with JS/app.js
@@ -1,20 +1,23 @@
 const input = document.getElementById('input');
 const button = document.getElementById('submit');
 
-const showhidebtn = document.getElementById('showhide-btn');
+const showhidebtn = document.getElementById('showhide-btn');//this for the challenge
 
 button.addEventListener('click', () => {
     let listItem = document.createElement('li');
-    let list = document.getElementsByTagName('ul')[0];
+    let list = document.getElementsByTagName('ul')[0];//list variable will get the unordered list index 0 - the first uL it finds
     listItem.textContent = input.value;
     list.appendChild(listItem);
-    input.value = '';
+    input.value = '';//this will add the item content to the bottom (appendChild)
 })
 
-//challenge cont'd
-//show/hide list
-//in console, type list.style to find object properties
-//then use list.style.display, it will show 'block' so you can show none
+// //
+
+// //challenge cont'd
+// //show/hide list
+// //in console, type list.style to find object properties
+// //then use list.style.display, it will show 'block' so you can show none
+
 showhidebtn.addEventListener("click", () => {
     let list = document.getElementsByTagName('ul')[0];
     if(list.style.display == 'none') {
@@ -24,5 +27,5 @@ showhidebtn.addEventListener("click", () => {
         list.style.display = 'none';
         showhidebtn.textContent = 'show';
     }
-})
-//inspect the html you can then see inline style as display:"none" added in when button clicked
+}
+)
