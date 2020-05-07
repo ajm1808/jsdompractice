ea9ca9b564b9f766d8d8962d38492a3ccfe88f8a
ajm1808 <ajm180885@gmail.com>
2020-05-07 16:05:31 +0100

    keycode

--- mc-5-1-js-dom-starterfiles/keycodechallenge/app.js
+++ mc-5-1-js-dom-starterfiles/keycodechallenge/app.js
@@ -1,3 +1,11 @@
+const eKey=document.getElementsByClassName("key-description");
+
+const codeD=document.getElementsByClassName("code-description");
+
+const eWhich=document.getElementsByClassName("which-description");
+
+const input=document.querySelector("input");
+
 
 
 
@@ -6,3 +14,36 @@ function keycode(){
     console.log(key);
     }
 
+    // button.addEventListener('click', () => {
+    //     let listItem = document.createElement('li');
+    
+    //     let list = document.getElementsByTagName('ul')[0];
+    //     listItem.textContent = input.value;
+        
+    //     list.appendChild(listItem);
+    
+    //     input.value = '';
+    // })
+    
+    // removeBtn.addEventListener('click', () => {
+    //     let listItem = document.querySelector('li:last-child');
+    //     let list = document.getElementsByTagName('ul')[0];
+    
+    //     list.removeChild(listItem);
+    // })
+    
+    // /*New code below*/
+    
+    // document.addEventListener("click",(event)=>{
+    //     console.log(event);
+    //     console.log(event.target);
+    // })
+    
+    // list.addEventListener("mouseover",()=>{
+    //     if(event.target.tagName=="LI"){event.target.textContent=event.target.textContent.toUpperCase();}
+    // })
+    // list.addEventListener("mouseout",()=>{
+    //     if(event.target.tagName=="LI"){event.target.textContent=event.target.textContent.toLowerCase();}
+    // })
+    
+    // //have to add the if to the above - otherwise it removes the list functionality. It only applies changes to LI rather than Ul
