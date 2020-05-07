24a98e680b78a45eda8cf9c23db67785d28b2d94
ajm1808 <ajm180885@gmail.com>
2020-05-07 16:58:08 +0100

    keycode

--- mc-5-1-js-dom-starterfiles/keycodechallenge/app.js
+++ mc-5-1-js-dom-starterfiles/keycodechallenge/app.js
@@ -1,49 +1,13 @@
-const eKey=document.getElementsByClassName("key-description");
-
-const codeD=document.getElementsByClassName("code-description");
-
-const eWhich=document.getElementsByClassName("which-description");
-
-const input=document.querySelector("input");
 
 
+    const key1 = document.getElementById("key1");
+const key2 = document.getElementById("key2");
+const key3 = document.getElementById("key3");
+const input = document.getElementById("keycode");
 
-
-function keycode(){
-    let key = event.keyCode;
-    console.log(key);
-    }
-
-    // button.addEventListener('click', () => {
-    //     let listItem = document.createElement('li');
-    
-    //     let list = document.getElementsByTagName('ul')[0];
-    //     listItem.textContent = input.value;
-        
-    //     list.appendChild(listItem);
-    
-    //     input.value = '';
-    // })
-    
-    // removeBtn.addEventListener('click', () => {
-    //     let listItem = document.querySelector('li:last-child');
-    //     let list = document.getElementsByTagName('ul')[0];
-    
-    //     list.removeChild(listItem);
-    // })
-    
-    // /*New code below*/
-    
-    // document.addEventListener("click",(event)=>{
-    //     console.log(event);
-    //     console.log(event.target);
-    // })
-    
-    // list.addEventListener("mouseover",()=>{
-    //     if(event.target.tagName=="LI"){event.target.textContent=event.target.textContent.toUpperCase();}
-    // })
-    // list.addEventListener("mouseout",()=>{
-    //     if(event.target.tagName=="LI"){event.target.textContent=event.target.textContent.toLowerCase();}
-    // })
-    
-    // //have to add the if to the above - otherwise it removes the list functionality. It only applies changes to LI rather than Ul
+document.addEventListener("keydown",(event) => {
+    key1.textContent=event.key;
+    key2.textContent=event.code;
+    key3.textContent=event.which;
+    input.textContent=(`This key code is ${event.keyCode}`);
+});
