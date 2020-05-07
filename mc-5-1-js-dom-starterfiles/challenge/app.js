9ad03b14554f30eeea4072608ec3c4362645c114
ajm1808 <ajm180885@gmail.com>
2020-05-07 14:32:57 +0100

    keycode challenge

--- mc-5-1-js-dom-starterfiles/challenge/app.js
+++ mc-5-1-js-dom-starterfiles/challenge/app.js
@@ -1,8 +1,5 @@
 
-// const newPara=document.createElement('p');
-// newPara.textContent='added with js';
 
-// document.querySelector('body').appendChild(newPara);
 
 function keycode(){
     let key = event.keyCode;
