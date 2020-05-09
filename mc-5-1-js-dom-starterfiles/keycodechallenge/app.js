75b7f0c5262e359f343edf9085f6a642cc61c748
ajm1808 <ajm180885@gmail.com>
2020-05-09 14:44:43 +0100

    keycode game

--- mc-5-1-js-dom-starterfiles/keycodechallenge/app.js
+++ mc-5-1-js-dom-starterfiles/keycodechallenge/app.js
@@ -5,9 +5,22 @@ const key2 = document.getElementById("key2");
 const key3 = document.getElementById("key3");
 const input = document.getElementById("keycode");
 
+// const wrap=document.getElementsByClassName("wrap");
+
+
+// document.addEventListener("keydown",(event)=>{
+//     if(InputEvent.style.display=="none"){
+//         wrap.style.display="block";
+//     }
+//     else {
+//         wrap.style.display="none";
+//     }
+// })
+
 document.addEventListener("keydown",(event) => {
     key1.textContent=event.key;
     key2.textContent=event.code;
     key3.textContent=event.which;
     input.textContent=(`This key code is ${event.keyCode}`);
 });
+
