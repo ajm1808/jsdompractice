180cd2a92746b4bbb35880261fe4a132f6661ec3
ajm1808 <ajm180885@gmail.com>
2020-05-11 15:30:47 +0100

    amended keycode

--- mc-5-1-js-dom-starterfiles/keycodechallenge/app.js
+++ mc-5-1-js-dom-starterfiles/keycodechallenge/app.js
@@ -5,6 +5,8 @@ const key2 = document.getElementById("key2");
 const key3 = document.getElementById("key3");
 const input = document.getElementById("keycode");
 
+document.querySelector(".wrap").style.  visibility="hidden";
+
 // const wrap=document.getElementsByClassName("wrap");
 
 
@@ -22,5 +24,6 @@ document.addEventListener("keydown",(event) => {
     key2.textContent=event.code;
     key3.textContent=event.which;
     input.textContent=(`This key code is ${event.keyCode}`);
+    document.querySelector(".wrap").style.visibility="hidden";
 });
 
